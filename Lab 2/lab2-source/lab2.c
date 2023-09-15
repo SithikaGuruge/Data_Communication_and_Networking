b5f815b75a9222ff4e903ab8c443eaa5bb9c5e9b
Sithika Guruge <sithikaguruge2001@gmail.com>
2023-09-15 15:49:38 +0530

    lab 2 updated

--- Lab 2/lab2-source/lab2.c
+++ Lab 2/lab2-source/lab2.c
@@ -5,50 +5,45 @@
 // user-defined header files
 #include "mypcm.h"
 
+// test
 
 // function prototypes
-void run(asignal * inputsignal);
-
+void run(asignal *inputsignal);
 
 int main()
 {
-    asignal * inputsignal = (asignal *) malloc(sizeof(asignal));
+    asignal *inputsignal = (asignal *)malloc(sizeof(asignal));
     run(inputsignal);
-    
-    //call any other function here
+
+    // call any other function here
 
     free(inputsignal);
-	return 0;
+    return 0;
 }
 
-void run(asignal * inputsignal) 
+void run(asignal *inputsignal)
 {
 
-    int interval,encorder_bits;
-    int A,omega,sigma,duration;
+    int interval, encorder_bits;
+    int A, omega, sigma, duration;
 
-
-    scanf("%d %d %d %d %d %d",&A,&omega,&sigma,&duration,&interval,&encorder_bits);
-
+    scanf("%d %d %d %d %d %d", &A, &omega, &sigma, &duration, &interval, &encorder_bits);
 
     inputsignal->A = A;
     inputsignal->omega = omega;
     inputsignal->sigma = sigma;
     inputsignal->duration = duration;
 
-    float * samples = malloc(sizeof(float)*inputsignal->duration/interval);
-    int * pcmpulses = malloc(sizeof(int)*inputsignal->duration/interval );
-    int * dsignal = malloc(sizeof(int)*inputsignal->duration/interval*inputsignal->duration);
+    float *samples = malloc(sizeof(float) * inputsignal->duration / interval);
+    int *pcmpulses = malloc(sizeof(int) * inputsignal->duration / interval);
+    int *dsignal = malloc(sizeof(int) * inputsignal->duration / interval * inputsignal->duration);
 
-    sampler(samples,interval,*inputsignal);
-    quantizer(samples,pcmpulses,pow(2,encorder_bits),inputsignal->A);
-    encoder(pcmpulses,dsignal,encorder_bits);
+    sampler(samples, interval, *inputsignal);
+    quantizer(samples, pcmpulses, pow(2, encorder_bits), inputsignal->A);
+    encoder(pcmpulses, dsignal, encorder_bits);
 
-    for (int j = 0; j < inputsignal->duration/interval*encorder_bits + encorder_bits; j++){
-        printf("%d",dsignal[j]);
-
+    for (int j = 0; j < inputsignal->duration / interval * encorder_bits + encorder_bits; j++)
+    {
+        printf("%d", dsignal[j]);
     }
-	
 }
-
-
